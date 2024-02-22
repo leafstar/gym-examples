51c3284aa0e2eabbed89d6e3f65e99259a30ffdb
muxingwang <954226839wmx@gmail.com>
2024-02-22 13:08:45 -0500

    many small changes

--- env_hetero/parta.py
+++ env_hetero/parta.py
@@ -8,7 +8,8 @@ from env_hetero import utils
 
 
 def chi(t, E):
-    return t - np.floor(t/E)*E
+    return int(np.floor(t / E) * E)
+
 
 # class BlockType(Enum):
 #     FLOOR = 1
@@ -119,7 +120,7 @@ class SimpleFrozenLake:
 
 
 class Agent:
-    def __init__(self, lam, Q0, P, reward_matrix, gamma=0.95, E=1, T=30000):
+    def __init__(self, lam, Q0, P, reward_matrix, gamma=0.99, E=1, T=30000):
         self.name = "RLagent"
         self.lam = lam
         self.Q = Q0
@@ -132,7 +133,7 @@ class Agent:
 
     def reset(self):
         self.steps = 0
-        self.lam = 1
+        self.lam = 0.9
 
     def getV(self):
         V = np.zeros((int(self.Q.shape[0] / 4), 1))
@@ -173,64 +174,78 @@ class Agent:
         # return self.Q
 
     def schedule_hyperparameters(self, k, forstar):
-        # self.alpha = 1 / (1 + np.log(1 + k))
-        # self.alpha = 0.9**k
-        # self.lam = np.log(k+1) / (k+1)
-        # self.lam = 1/(k+1)
-        # self.lam = 0.1
         if forstar:
-            self.lam = 0.1
+            self.lam = 0.99
         else:
             # self.lam = 1 / np.sqrt((k+1))
             # self.lam = 1 / np.sqrt(self.T)
             # self.lam = 1 / (k + 1)
-            # self.lam = 0.1
+
+            self.lam = 0.99
+            if self.steps>150:
+                self.lam = 1/np.sqrt(self.steps)
             # self.lam = self.E/(k+1)
             # self.lam = 1/(k + self.E)
             # self.lam = np.log(self.T + 1)**2 / (self.T + 1)
-            self.lam = np.log(k+1)/(k + 1)
+            # self.lam = min(1, np.log(k + 1) / (k + 1) / np.sqrt((1 - self.gamma)))
+            # self.lam = np.log(k + 1) / (k + 1)
             # self.lam = np.log(k + 1) **2/ (k + 1)
             # self.lam = 1 / self.T
 
 
 def train(agents, imagin_env, Qstar, kappa, max_time_step, E=1):
+    K = len(agents)
     t = 0
     maxT = max_time_step
     total_comm_num = 0
     Deltas = []
     Delta_sync_rounds = []
-    local_Delta_tk = []
+    local_Delta_tk = []  # Delta_t^k
+    local_stepwise = []  # V_{j+1}-V_j
     Qtbar_list = []
+    lam_list = []
+    Vik = []  # a collection of Vik
     # initialize Q_0^k
-    for agent in agents:
-        agent.Q = np.zeros_like(agent.Q)
-        agent.reset()
-    Q_t_bar = np.mean([agent.Q for agent in agents], axis=0)
-    Q_t_bar_old = Q_t_bar
+    for ag in agents:
+        ag.Q = np.zeros_like(ag.Q)
+        ag.reset()
+    # append the quantities at time 0
+    Q_t_bar = np.mean([ag.Q for ag in agents], axis=0)
     Delta_0 = np.linalg.norm(Qstar - Q_t_bar, np.inf)
     Deltas.append(Delta_0)
+    Delta_sync_rounds.append(np.linalg.norm(Qstar - Q_t_bar, np.inf))
     local_Delta_tk.append(np.linalg.norm(Qstar - agents[0].Q, np.inf))
+    local_stepwise.append(0)
+    # quantities that need to be tracked during the training.
+    # in other words, these quantities need to be compared with themselves.
+    Vk_old = [ag.getV() for ag in agents]
+    Vik.append(Vk_old)
+    Q_t_bar_old = Q_t_bar
     while True:
         Qtbar_list.append(Q_t_bar)
         t = t + 1
-        for agent in agents:  # all agents local update
-            agent.updateQ(forstar=False)  # Q_t^k
+        for ag in agents:  # all agents local update
+            ag.updateQ(forstar=False)  # Q_t^k
+        lam_list.append(agents[0].lam)
         Q_t_bar = np.mean([agent.Q for agent in agents], axis=0)  # Q_t bar
-
         Delta_t_norm = np.linalg.norm(Qstar - Q_t_bar, np.inf)
-        # print(Delta_t_norm)
         Deltas.append(Delta_t_norm)
-        local_Delta_tk.append(np.linalg.norm(Qstar - agents[0].Q, np.inf))
+        local_Delta_tk.append(np.mean([np.linalg.norm(Qstar - agents[i].Q, np.inf) for i in range(K)]))
+        local_stepwise.append(np.mean([np.linalg.norm(agents[i].getV() - Vk_old[i], np.inf) for i in range(K)]))
+        if t>150:
+            E = 10
         if t % E == 0:  # synchronization step
             total_comm_num += 1
             Delta_sync_rounds.append(Delta_t_norm)
             for agent in agents:
                 agent.Q = Q_t_bar
-        print(t, Delta_t_norm)
-        if Delta_t_norm <= 10e-5 or np.linalg.norm(Q_t_bar_old-Q_t_bar,np.inf)<=10e-10  or t > maxT:
+        # print(t, Delta_t_norm)
+        if Delta_t_norm <= 10e-5 or np.linalg.norm(Q_t_bar_old - Q_t_bar, np.inf) <= 10e-10 or t > maxT:
             break
         Q_t_bar_old = Q_t_bar
-    return Deltas, t, total_comm_num, Qtbar_list, Delta_sync_rounds, local_Delta_tk
+        Vk_old = [agents[i].getV() for i in range(K)]
+        Vik.append(Vk_old)
+    return Deltas, t, total_comm_num, Qtbar_list, Delta_sync_rounds, local_Delta_tk, local_stepwise, lam_list, Vik
 
 
 def analytical(t, delta0, gamma):
@@ -252,15 +267,15 @@ def analytical(t, delta0, gamma):
 
     # print(t,np.prod(1-np.array([lam(i) for i in range(t)])*(1-gamma)),delta0)
     return np.prod(1 - np.array([lam(i) for i in range(t)]) * (1 - gamma)) * delta0
-    #return 32/3/(1-gamma)**2*(np.exp(-0.05*np.sqrt(lam(i)*30000)))
+    # return 32/3/(1-gamma)**2*(np.exp(-0.05*np.sqrt(lam(i)*30000)))
 
 
 if __name__ == '__main__':
     mpl.use('macosx')
     random.seed(2)
     K = 5
-    maxT = 30000
-    E = np.floor(np.log(maxT))
+    maxT = 50000
+    E =  maxT+1  # maxT+1  # 1  # np.floor(np.log(maxT)) #
     Pk_list = []  # a list of all local transition prob matrix
     Qkstar_list = []  # a list of all local optimal Q matrix
     Vkstar_list = []  # a list of all local optimal V matrix
@@ -270,14 +285,14 @@ if __name__ == '__main__':
     num_s = grid_size ** 2
     a_dim = 4  # U D L R
     agent_list = []
-    R = np.zeros((num_s * a_dim, 1))
+    R = np.ones((num_s * a_dim, 1))
     for k in range(K):
         Env = SimpleFrozenLake(grid_size)  # initialize the envs
         print(f"maze {k}")
         print_maze(Env.grid)
         Pk = Env.P  # get the transition matrix of env k
         Pk_list.append(Pk)  # add the transition matrix of env k to a list
-        R = Env.R
+        # R = Env.R  this line is a simple reward function. only two entries are one.
         agentk = Agent(lam=1, Q0=np.zeros((num_s * a_dim, 1)), P=Pk, reward_matrix=R)
         agent_list.append(agentk)
         Delta_tk, local_Qt, local_Vt = agentk.learn(forstar=True)  # Delta_tk is the local one-step error
@@ -304,18 +319,28 @@ if __name__ == '__main__':
     """
     Animation of agent 1 approaches to the optimal Q.
     Plot of Qstar(Img Env), Qkstar(Local) and average of Qkstar.
-    Result is the averaged local optimal Q is far away from the optimal Q in the imainary env.
+    Result is the averaged local optimal Q is far away from the optimal Q in the imaginary env.
     """
     utils.plotQseq(local_Qt_k[0], Qstar, Qkstar_list[0])
-    # plt.figure()
-    # colors = plt.cm.viridis(np.linspace(0, 1, K+2))
-    # plt.plot(Qstar, 'o', color = colors[0], label="Qstar")
-    # plt.plot(np.mean(Qkstar_list, axis=0), 'o', color="red", label="Qkstar avg")
-    # for i, Qkstar in enumerate(Qkstar_list):
-    #     plt.plot(Qkstar, 'o', color=colors[i+1], alpha=0.2, label=f"Q{i}_star")
-    # plt.legend()
-    # plt.savefig("Qstars.pdf", format="pdf", bbox_inches="tight")
-    # plt.show()
+    plt.figure()
+    plt.ylabel("the difference to optimal Q")
+    plt.plot(range(len(Qt_list)), [np.linalg.norm(Qstar - Qt_list[i], np.inf) for i in range(len(Qt_list))],label=f"agent img converges with t={len(Qt_list)}")
+    for i in range(K):
+        plt.plot(range(len(local_Qt_k[i])),
+             [np.linalg.norm(Qkstar_list[i] - local_Qt_k[i][j], np.inf) for j in range(len(local_Qt_k[i]))],label=f"agent {i} converges with t={len(local_Qt_k[i])}")
+    plt.legend()
+
+    plot_local_Qs_and_Qstar = False
+    if plot_local_Qs_and_Qstar:
+        plt.figure()
+        colors = plt.cm.viridis(np.linspace(0, 1, K+2))
+        plt.plot(Qstar, 'o', color = colors[0], label="Qstar")
+        plt.plot(np.mean(Qkstar_list, axis=0), 'o', color="red", label="Qkstar avg")
+        for i, Qkstar in enumerate(Qkstar_list):
+            plt.plot(Qkstar, 'o', color=colors[i+1], alpha=0.2, label=f"Q{i}_star")
+        plt.legend()
+        plt.savefig("Qstars.pdf", format="pdf", bbox_inches="tight")
+        plt.show()
 
     """
     Above is the fixed-points analysis, and from now on is the iterative analysis.
@@ -339,21 +364,81 @@ if __name__ == '__main__':
             kappa = np.linalg.norm(Pbar - Pk, np.inf)
     print(f"kappa = {kappa}")
 
-    Deltas, T, comm_num, Qtbar_list, Deltas_sync, local_Delta_tk = train(agent_list, Pbar, Qstar=Qstar, kappa=kappa, max_time_step=maxT,
-                                                         E=E)
+    Deltas, T, comm_num, Qtbar_list, Deltas_sync, local_Delta_tk, local_stepwise, lams, Vik = train(agent_list, Pbar,
+                                                                                               Qstar=Qstar,
+                                                                                               kappa=kappa,
+                                                                                               max_time_step=maxT,
+                                                                                               E=E)
     print("computed\n")
-    utils.plotQseq(Qtbar_list, Qstar, Qstar, Deltas)
+    # utils.plotQseq(Qtbar_list, Qstar, None, Deltas)
     plt.figure()
     # analytical_rate = [analytical(i, np.linalg.norm(Qstar, np.inf), gamma=agent_global.gamma) for i in
     #                    range(min(maxT, T))]
-    plt.title(r"$\lambda_t = \frac{1}{\sqrt{T}}$")
-    #plt.plot(range(min(maxT, T)), analytical_rate, color='red', label="analytical")
-    plt.plot(range(len(Deltas)), Deltas, color="black", linestyle="dotted",
-             label=f"fed decay to 0 {T}, err={Deltas[-1]}")
-    plt.plot(range(len(local_Delta_tk)), local_Delta_tk, alpha=0.2, color="purple", linestyle="dotted",
-             label=f"local_Delta_tk")
-    plt.scatter(range(0, len(Deltas_sync), int(E)), Deltas_sync, s=1, color="orange", label=f"sync_err")
+    plt.title(r"$\lambda_t = \frac{log(t)}{t}$")
+    # plt.plot(range(min(maxT, T)), analytical_rate, color='red', label="analytical")
+    plt.scatter(range(len(Deltas)), Deltas, s=2, color="black", linestyle="dotted",
+                label=f"fed decay to 0 {T}, err={Deltas[-1]}")
+    plt.scatter(range(len(local_Delta_tk)), local_Delta_tk, s=1, alpha=0.8, color="red", linestyle="dotted",
+                label=f"local_Delta_tk")
+    plt.scatter(range(len(local_stepwise)), local_stepwise, s=1, alpha=0.8, color="blue", linestyle="dotted",
+                label=r"max norm of $V_j-V_{j+1}$")
+    #plt.scatter(range(0, T, int(E)), Deltas_sync, s=1, color="orange", label=f"sync_err")
     plt.ylim((0, max(Deltas)))
     plt.legend()
+
+    """
+    Diagnostic section!!!!!!!
+    """
+    e22 = []
+    e22_b = []
+    e22_wo_p = []
+    e22_norm_inside = []
+    for i in range(T):
+        e22_bs = np.linalg.norm(
+            np.mean([np.matmul(Pk_list[k], (np.array(Vik[chi(i, E)][k]) - np.array(Vik[i][k]))) for k in range(K)],
+                    axis=0),
+            np.Inf)
+        e22i = np.linalg.norm(np.mean([np.matmul(Pk_list[k], (Vstar - np.array(Vik[i][k]))) for k in range(K)], axis=0),
+                              np.Inf)
+        e22_norm_inside.append(np.mean([np.linalg.norm(np.matmul(Pk_list[k], (Vstar - np.array(Vik[i][k]))),np.Inf) for k in range(K)], axis=0)
+                              )
+        e22_wo_p.append(
+            np.mean([np.linalg.norm((np.array(Vik[chi(i, E)][k]) - np.array(Vik[i][k])), np.Inf) for k in range(K)],
+                    axis=0)
+            )
+        e22_b.append(e22_bs)
+        e22.append(e22i)
+    plt.figure()
+    plt.title("diagnostics")
+    #plt.scatter(range(len(Deltas)), Deltas, s=2, color="black", label=r"$\Delta_{t+1}$")
+    #plt.scatter(range(len(lams)), lams, s=2, color="blue", label=r"$\lambda_t$")
+    #plt.scatter(range(1, len(Deltas)), (1 - np.array(lams)) * np.array(Deltas[:len(Deltas) - 1]), s=2, color="orange",
+    #            label=r"$(1-\lambda_t)\Delta_{t}$")
+    plt.scatter(range(len(Deltas)), Deltas, s=12, color="black", linestyle="dotted",
+                label=f"fed decay to 0 {T}, err={Deltas[-1]}")
+    #plt.scatter(range(0, T, int(E)), Deltas_sync, s=10,alpha=0.8, color="orange", label=r"sync_err $\|\Delta_{\chi(i)}\|$")
+    plt.scatter(range(len(e22)), e22, s=2, color="violet", label = r"$\|\frac{1}{K}\sum_{i=1}^K{P^k(V^*-V_i^k)}\|$") # norm(average (Vstar - Vtk))
+    plt.scatter(range(len(e22_wo_p)), e22_wo_p, s=10,alpha=0.8, color="purple",
+                label=r"$\frac{1}{K}\sum_{i=1}^K\|{(V_{\chi(i)}-V_i^k)}\|$")  # norm(average (Vchi(t) - Vtk))
+    plt.scatter(range(len(e22_b)), e22_b, s=6, alpha=0.8, color="yellow", label=r"$\|\frac{1}{K}\sum_{i=1}^K{P^k(V_{\chi(i)}-V_i^k)}\|$") # norm(average (Vchi(t) - Vtk))
+    plt.scatter(range(len(e22_norm_inside)), e22_norm_inside, s=1, alpha=0.8, color="crimson", linestyle="dotted",
+                label=r"$\frac{1}{K}\sum_{i=1}^K\|{P^k(V^*-V_i^k)}\|$")
+
+    plt.scatter(range(len(local_stepwise)), local_stepwise, s=1, alpha=0.8, color="blue", linestyle="dotted",
+                label=r"max norm of $V_j-V_{j+1}$")
+    plt.legend()
     print(Deltas[-1])
-    plt.show(block=True)
+   # print(utils.lambda_analysis(lams, 5000))
+    plt.show()
+
+    #### check V diff
+    plt.figure()
+    t_v =9
+    plt.title(f"time {t_v}")
+    plt.ylim([-3,np.max(Vik)+5])
+    colors = ["red","black","orange","violet","blue"]
+    for i in range(K):
+        plt.scatter(range(25),Vik[t_v][i],s = (K-i)*20, color = colors[i],alpha = 1, label = f"V^{i}")
+    plt.scatter(range(25),Vstar, s = 100,marker=',',alpha = 0.5, label = f"Vstar")
+    plt.legend()
+    d = 1
--- env_hetero/utils.py
+++ env_hetero/utils.py
@@ -6,7 +6,7 @@ from matplotlib.animation import FuncAnimation
 import time
 
 
-def plotQseq(Qtk_list, Qstar, Qkstar, Deltas=None):
+def plotQseq(Qtk_list, Qstar, Qkstar=None, Deltas=None):
     # creating initial data values
     # of x and y
     mpl.use('macosx')
@@ -18,7 +18,7 @@ def plotQseq(Qtk_list, Qstar, Qkstar, Deltas=None):
 
     # here we are creating subplots
     figure, ax = plt.subplots(figsize=(10, 8))
-    line1, = ax.plot(x, y, 'o', label="Qtk")
+    line1, = ax.plot(x, y, 'o', label=r"$\barQ_t^k$")
     ax.ylim = np.max(Qtk_list)
     # setting title
     plt.title("Convergence of Q learning", fontsize=20)
@@ -26,8 +26,9 @@ def plotQseq(Qtk_list, Qstar, Qkstar, Deltas=None):
     # setting x-axis label and y-axis label
     plt.xlabel("X-axis")
     plt.ylabel("Y-axis")
-    plt.plot(Qkstar, 'o', color='red', alpha=0.8, label=f"Q0 star")
-    plt.plot(Qstar, 'o', color='green', alpha=0.2, label=f"Q star")
+    if Qkstar is not None:
+        plt.plot(Qkstar, 'o', color='red', alpha=0.8, label=f"Q0 star")
+    plt.plot(Qstar, 'o', color='indigo', alpha=0.8, label=f"Q star")
     L = plt.legend()
     # Loop
     for i, Qtk in enumerate(Qtk_list):
@@ -38,9 +39,9 @@ def plotQseq(Qtk_list, Qstar, Qkstar, Deltas=None):
         line1.set_xdata(x)
         line1.set_ydata(new_y)
         if Deltas is not None:
-            L.get_texts()[0].set_text(f"Qtk, t = {i}, Delta = {Deltas[i]}")
+            L.get_texts()[0].set_text(r"$\bar Q_t^k$," + f" t = {i}, Delta = {Deltas[i]}")
         else:
-            L.get_texts()[0].set_text(f"Qtk, t = {i}")
+            L.get_texts()[0].set_text(r"$\bar Q_t^k$," + f" t = {i}")
         line1.set_alpha(0.5)
 
         # drawing updated values
@@ -56,6 +57,7 @@ def plotQseq(Qtk_list, Qstar, Qkstar, Deltas=None):
         # else:
         #     time.sleep(0.002)
 
+
 def analytical(t):
     """
     Analytical solution for exponential decay for this recurrence relation.
@@ -70,15 +72,32 @@ def analytical(t):
 
     gamma = 0.99
     a = 1
-    lam = np.log(t) ** a / t**0.8
+    lam = np.log(t) ** a / t ** 0.8
     E = np.log(t)
     kappa = 1.2
-    res1 = 32/3/(1-gamma)**2*np.exp(-0.5 * np.sqrt((1 - gamma) * t * lam))
-    res2 = 32/3/(1-gamma)**2*gamma**2*kappa*lam*E
-    res3 = 16*E/(1-gamma)**3/(t+E)
+    res1 = 32 / 3 / (1 - gamma) ** 2 * np.exp(-0.5 * np.sqrt((1 - gamma) * t * lam))
+    res2 = 32 / 3 / (1 - gamma) ** 2 * gamma ** 2 * kappa * lam * E
+    res3 = 16 * E / (1 - gamma) ** 3 / (t + E)
     # print(t,np.prod(1-np.array([lam(i) for i in range(t)])*(1-gamma)),delta0)
     # return np.prod(1 - np.array([lam(i) for i in range(t)]) * (1 - gamma)) * delta0
-    return res1,res2,res3
+    return res1, res2, res3
+
+
+def lambda_analysis(lambdas, mid):
+    res = 0
+    t = len(lambdas)
+    # for i in range(mid + 1):
+    #     prod = 1
+    #     for j in range(i + 1, t):
+    #         prod *= 1 - lambdas[j]
+    #     res += lambdas[i] * prod
+    res2 = 0
+    for i in range(mid + 1, t):
+        prod = 1
+        for j in range(i + 1, t):
+            prod *= 1 - lambdas[j]
+        res2 += lambdas[i] * prod
+    return res, res2
 
 
 if __name__ == "__main__":
